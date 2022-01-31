e739b62afa6883cc13503b63903cea8ea2246e45
Adam <monster.eat90@gmail.com>
2022-01-31 20:27:07 +0700

    adding curve navbar in homePage

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,21 +1,53 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:list_list_2/page/login/login_page.dart';
+// ignore_for_file: prefer_const_constructors
 
-class HomePage extends StatelessWidget {
-  final _auth = FirebaseAuth.instance;
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:list_list_2/constants/color_constant.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  //Navbar Icon
+  final pages = <Widget>[
+    Icon(Icons.home, size: 30),
+    Icon(Icons.search, size: 30),
+    Icon(Icons.collections, size: 30),
+    Icon(Icons.person, size: 30),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
+      backgroundColor: defaultColor,
+      appBar: AppBar(
+        title: Text("List List"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: index,
+        height: 55,
+        items: pages,
+        backgroundColor: Colors.transparent,
+        color: powderBlue,
+        buttonBackgroundColor: blueGrey,
+        onTap: (index) => setState(() => this.index = index),
+      ),
       body: Center(
-        child: FlatButton(
-          child: Text('Logout'),
-          onPressed: () {
-            _auth.signOut();
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => LoginPage()));
-          },
+        child: Text(
+          '$index',
+          style: GoogleFonts.dongle(
+            fontSize: 125,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
